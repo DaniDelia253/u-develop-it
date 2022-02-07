398130e8271e41514c49fc93613203cb8e463c62
DaniDelia253 <90367091+DaniDelia253@users.noreply.github.com>
2022-02-06 22:42:46 -0600

    add more db requests

--- server.js
+++ server.js
@@ -22,7 +22,35 @@ const db = mysql.createConnection(
 
 
 db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows)
+    // console.log(rows)
+})
+
+//GET a single candidate
+db.query(`SELECT * FROM candidates where id = 1`, (err, row) => {
+    if (err) {
+        console.log(err)
+    }
+    // console.log(row)
+})
+
+//Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, Last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(result)
 })
 
 //default response for any other request (Not Found)
